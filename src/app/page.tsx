daab34c31b8b12d26f114f60db546384c8cb642c
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2025-04-08 19:39:56 +0200

    upconfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <><h1>Home</h1></>
+    <>
+    <div className="bg-gray-600 w-full h-screen flex flex-col items-center justify-center">
+      <h1>Home</h1>
+    </div>
+    </>
     
   );
 }
