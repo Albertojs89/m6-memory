05c8418e52fd8dea3de647767e96e0a96edbc09c
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2025-05-06 19:23:41 +0200

    header actualizado con vistas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import Link from "next/link"; // al principio del archivo
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      {/* Logo + Título */}
       <div className="flex items-center">
-        <img src="/brain.png" alt="Logo" className="h-20 w-20 mr-5" />
+        <img src="/brain.png" alt="Logo" className="h-20 w-20 mr-5 object-contain" />
         <h1 className="text-xl font-bold">Memory</h1>
       </div>
+
+      {/* Navegación */}
       <nav>
-        <ul className="flex space-x-4 mr-16 gap-16">
+        <ul className="flex items-center gap-10 mr-6 text-lg">
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
+          <li><Link href="/login">Login</Link></li>
+          <li><Link href="/register">Register</Link></li>
           <li>
             <Avatar>
               <AvatarImage src="/JessYBruma.png" />
@@ -18,10 +24,7 @@ export default function Header() {
             </Avatar>
           </li>
         </ul>
-        
-
       </nav>
     </header>
   );
 }
-  
