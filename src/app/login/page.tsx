521586f793b4ae99121f4f3a60d98685396c569f
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2025-05-20 20:24:27 +0200

    up2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-  const router = useRouter();
+  // const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
